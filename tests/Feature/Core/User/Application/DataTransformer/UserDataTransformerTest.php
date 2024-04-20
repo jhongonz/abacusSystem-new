1e25dd9c61a1c3820350eb6f810f28c8ebfc5bb0
Jhonny Andres Gonzalez <jhonnygonzalezf@gmail.com>
2024-04-21 00:18:21 +0200

    add test to data transformer

---
+++ tests/Feature/Core/User/Application/DataTransformer/UserDataTransformerTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Tests\Feature\Core\User\Application\DataTransformer;
+
+use Core\User\Application\DataTransformer\UserDataTransformer;
+use Core\User\Domain\User;
+use Core\User\Domain\ValueObjects\UserCreatedAt;
+use Core\User\Domain\ValueObjects\UserEmployeeId;
+use Core\User\Domain\ValueObjects\UserId;
+use Core\User\Domain\ValueObjects\UserLogin;
+use Core\User\Domain\ValueObjects\UserPassword;
+use Core\User\Domain\ValueObjects\UserPhoto;
+use Core\User\Domain\ValueObjects\UserProfileId;
+use Core\User\Domain\ValueObjects\UserState;
+use Core\User\Domain\ValueObjects\UserUpdatedAt;
+use DateTime;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Mockery\Mock;
+use PHPUnit\Framework\MockObject\Exception;
+use Tests\TestCase;
+
+class UserDataTransformerTest extends TestCase
+{
+    private User|Mock $user;
+    private UserDataTransformer $dataTransformer;
+
+    /**
+     * @throws Exception
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = $this->createMock(User::class);
+        $this->dataTransformer = new UserDataTransformer();
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->user, $this->dataTransformer);
+        parent::tearDown();
+    }
+
+    public function test_write_should_return_object(): void
+    {
+        $result = $this->dataTransformer->write($this->user);
+
+        $this->assertInstanceOf(UserDataTransformer::class, $result);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function test_read_should_return_array(): void
+    {
+        $datetime = new DateTime('2024-04-20 22:08:42');
+
+        $expected = [
+            'user' => [
+                'id' => 1,
+                'employeeId' => 1,
+                'profileId' => 1,
+                'login' => 'login',
+                'password' => '12345',
+                'state' => 1,
+                'photo' => 'test.jpg',
+                'createdAt' => $datetime,
+                'updatedAt' => $datetime,
+            ]
+        ];
+
+        $userIdMock = $this->createMock(UserId::class);
+        $userIdMock->expects(self::once())
+            ->method('value')
+            ->willReturn(1);
+        $this->user->method('id')->willReturn($userIdMock);
+
+        $userEmployeeIdMock = $this->createMock(UserEmployeeId::class);
+        $userEmployeeIdMock->expects(self::once())
+            ->method('value')
+            ->willReturn(1);
+        $this->user->method('employeeId')->willReturn($userEmployeeIdMock);
+
+        $userProfileIdMock = $this->createMock(UserProfileId::class);
+        $userProfileIdMock->expects(self::once())
+            ->method('value')
+            ->willReturn(1);
+        $this->user->method('profileId')->willReturn($userProfileIdMock);
+
+        $userLoginMock = $this->createMock(UserLogin::class);
+        $userLoginMock->expects(self::once())
+            ->method('value')
+            ->willReturn('login');
+        $this->user->method('login')->willReturn($userLoginMock);
+
+        $userPasswordMock = $this->createMock(UserPassword::class);
+        $userPasswordMock->expects(self::once())
+            ->method('value')
+            ->willReturn('12345');
+        $this->user->method('password')->willReturn($userPasswordMock);
+
+        $userStateMock = $this->createMock(UserState::class);
+        $userStateMock->expects(self::once())
+            ->method('value')
+            ->willReturn(1);
+        $this->user->method('state')->willReturn($userStateMock);
+
+        $userPhotoMock = $this->createMock(UserPhoto::class);
+        $userPhotoMock->expects(self::once())
+            ->method('value')
+            ->willReturn('test.jpg');
+        $this->user->method('photo')->willReturn($userPhotoMock);
+
+        $userCreatedAtMock = $this->createMock(UserCreatedAt::class);
+        $userCreatedAtMock->expects(self::once())
+            ->method('value')
+            ->willReturn($datetime);
+        $this->user->method('createdAt')->willReturn($userCreatedAtMock);
+
+        $userUpdatedAtMock = $this->createMock(UserUpdatedAt::class);
+        $userUpdatedAtMock->expects(self::once())
+            ->method('value')
+            ->willReturn($datetime);
+        $this->user->method('updatedAt')->willReturn($userUpdatedAtMock);
+
+        $this->dataTransformer->write($this->user);
+        $result = $this->dataTransformer->read();
+
+        $this->assertSame($expected, $result);
+        $this->assertIsArray($result);
+    }
+}
