5454fd9111c198527aacea44c5b5403a82f459d6
Jhonny Andres Gonzalez <jhonnygonzalezf@gmail.com>
2024-04-20 20:29:57 +0200

    test to domain value object user context

---
+++ tests/Feature/Core/User/Domain/ValueObjects/UserPasswordTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\Core\User\Domain\ValueObjects;
+
+use Core\User\Domain\ValueObjects\UserPassword;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserPasswordTest extends TestCase
+{
+    private UserPassword $valueObject;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->valueObject = new UserPassword('12345');
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->valueObject);
+        parent::tearDown();
+    }
+
+    public function test_value_should_return_string(): void
+    {
+        $expected = '12345';
+        $result = $this->valueObject->value();
+
+        $this->assertSame($expected, $result);
+        $this->assertIsString($result);
+    }
+
+    public function test_setValue_should_change_value_and_return_object(): void
+    {
+        $expected = 'abcde';
+        $original = $this->valueObject->value();
+        $object = $this->valueObject->setValue($expected);
+
+        $return = $this->valueObject->value();
+        $this->assertSame($expected, $return);
+        $this->assertInstanceOf(UserPassword::class, $object);
+        $this->assertIsString($return);
+        $this->assertNotEquals($expected, $original);
+    }
+}
