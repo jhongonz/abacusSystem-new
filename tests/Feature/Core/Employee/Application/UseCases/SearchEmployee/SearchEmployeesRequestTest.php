3e49e00b934e79791dfff711b780540ee6b63058
Jhonny Andres Gonzalez <jhonnygonzalezf@gmail.com>
2024-04-30 08:17:03 +0200

    complete test to use case search employee

---
+++ tests/Feature/Core/Employee/Application/UseCases/SearchEmployee/SearchEmployeesRequestTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Core\Employee\Application\UseCases\SearchEmployee;
+
+use Core\Employee\Application\UseCases\SearchEmployee\SearchEmployeesRequest;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\TestCase;
+
+#[CoversClass(SearchEmployeesRequest::class)]
+class SearchEmployeesRequestTest extends TestCase
+{
+    private SearchEmployeesRequest $request;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->request = new SearchEmployeesRequest();
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->request);
+        parent::tearDown();
+    }
+
+    public function test_filters_should_return_array(): void
+    {
+        $result = $this->request->filters();
+        $this->assertIsArray($result);
+    }
+}
---
+++ tests/Feature/Core/Employee/Application/UseCases/SearchEmployee/SearchEmployeesTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Feature\Core\Employee\Application\UseCases\SearchEmployee;
+
+use Core\Employee\Application\UseCases\DeleteEmployee\DeleteEmployeeRequest;
+use Core\Employee\Application\UseCases\SearchEmployee\SearchEmployees;
+use Core\Employee\Application\UseCases\SearchEmployee\SearchEmployeesRequest;
+use Core\Employee\Domain\Contracts\EmployeeRepositoryContract;
+use Core\Employee\Domain\Employees;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\MockObject\Exception;
+use PHPUnit\Framework\MockObject\MockObject;
+use Tests\TestCase;
+
+#[CoversClass(SearchEmployees::class)]
+class SearchEmployeesTest extends TestCase
+{
+    private SearchEmployeesRequest|MockObject $request;
+    private EmployeeRepositoryContract|MockObject $repository;
+    private SearchEmployees $useCase;
+
+    /**
+     * @throws Exception
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->repository = $this->createMock(EmployeeRepositoryContract::class);
+        $this->request = $this->createMock(SearchEmployeesRequest::class);
+        $this->useCase = new SearchEmployees($this->repository);
+    }
+
+    public function tearDown(): void
+    {
+        unset(
+            $this->request,
+            $this->repository,
+            $this->useCase
+        );
+        parent::tearDown();
+    }
+
+    /**
+     * @throws Exception
+     * @throws \Exception
+     */
+    public function test_execute_should_return_value_object(): void
+    {
+        $employeesMock = $this->createMock(Employees::class);
+
+        $this->request->expects(self::once())
+            ->method('filters')
+            ->willReturn([]);
+
+        $this->repository->expects(self::once())
+            ->method('getAll')
+            ->with([])
+            ->willReturn($employeesMock);
+
+        $result = $this->useCase->execute($this->request);
+
+        $this->assertInstanceOf(Employees::class, $result);
+        $this->assertSame($result, $employeesMock);
+    }
+
+    /**
+     * @throws Exception
+     * @throws \Exception
+     */
+    public function test_execute_should_return_null(): void
+    {
+        $this->request->expects(self::once())
+            ->method('filters')
+            ->willReturn([]);
+
+        $this->repository->expects(self::once())
+            ->method('getAll')
+            ->with([])
+            ->willReturn(null);
+
+        $result = $this->useCase->execute($this->request);
+
+        $this->assertNull($result);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function test_execute_should_return_exception(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Request not valid');
+
+        $requestMock = $this->createMock(DeleteEmployeeRequest::class);
+        $this->useCase->execute($requestMock);
+    }
+}
