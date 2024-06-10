132cd71f2a8376308d13ef765716a4222ec4dbdf
Jhonny Andres Gonzalez <jhonnygonzalezf@gmail.com>
2024-06-10 14:28:16 +0200

    code coverage ignore in middleware

--- app/Http/Middleware/OnlyAjaxRequest.php
+++ app/Http/Middleware/OnlyAjaxRequest.php
@@ -4,19 +4,31 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @codeCoverageIgnore
+ */
 class OnlyAjaxRequest
 {
+    private Redirector $redirector;
+
+    public function __construct(
+        Redirector $redirector
+    ) {
+        $this->redirector = $redirector;
+    }
+
     /**
      * Handle an incoming request.
      *
      * @param  Closure(Request): (Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next): Closure|Response
     {
         if (! $request->ajax()) {
-            return redirect()->route('index');
+            return $this->redirector->route('index');
         }
 
         return $next($request);
--- app/Http/Middleware/VerifySession.php
+++ app/Http/Middleware/VerifySession.php
@@ -3,29 +3,49 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @codeCoverageIgnore
+ */
 class VerifySession
 {
+    private Redirector $redirector;
+    private Session $session;
+
+    public function __construct(
+        Redirector $redirector,
+        Session $session
+    ) {
+        $this->redirector = $redirector;
+        $this->session = $session;
+    }
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param Closure(Request): (Response) $next
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (! session()->exists(['user', 'profile', 'employee'])) {
+        if (! $this->session->exists(['user', 'profile', 'employee'])) {
 
             if ($request->ajax()) {
-                return response()->json([
-                    'error' => 'unauthorized',
-                    'error_description' => 'Failed authentication',
-                    'reason' => 401,
-                ], 401);
+                return new JsonResponse(
+                    [
+                        'error' => 'unauthorized',
+                        'error_description' => 'Failed authentication',
+                        'reason' => 401,
+                    ],
+                    Response::HTTP_UNAUTHORIZED
+                );
             }
 
-            return redirect('/');
+            return $this->redirector->to('/');
         }
 
         return $next($request);
