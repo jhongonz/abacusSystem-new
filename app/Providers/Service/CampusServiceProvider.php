7bd184f6ef653cc2de79fdbe2329ed19f7ebfc1b
Jhonny Andres Gonzalez <jhonnygonzalezf@gmail.com>
2024-06-17 13:42:27 +0200

    add data transformer campus

--- app/Providers/Service/CampusServiceProvider.php
+++ app/Providers/Service/CampusServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers\Service;
 
+use Core\Campus\Application\DataTransformer\CampusDataTransformer;
 use Core\Campus\Application\Factory\CampusFactory;
+use Core\Campus\Domain\Contracts\CampusDataTransformerContract;
 use Core\Campus\Domain\Contracts\CampusFactoryContract;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,7 +14,8 @@ class CampusServiceProvider extends ServiceProvider
      * All the container bindings that should be registered
      */
     public array $singletons = [
-        CampusFactoryContract::class => CampusFactory::class
+        CampusFactoryContract::class => CampusFactory::class,
+        CampusDataTransformerContract::class => CampusDataTransformer::class
     ];
 
     /**
