2c88a84d497f7c888b1b9ac3c119289592916962
Jhonny Andres Gonzalez <jhonnygonzalezf@gmail.com>
2024-06-25 08:20:29 +0200

    continue creating testing to campsu domain

---
+++ tests/Feature/Core/Campus/Application/DataProvider/CampusDataTransformerDataProvider.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @author Jhonny Andres Gonzalez <jhonnygonzalezf@gmail.com>
+ * Date: 2024-06-24 08:41:02
+ */
+
+namespace Tests\Feature\Core\Campus\Application\DataProvider;
+
+final class CampusDataTransformerDataProvider
+{
+    public static function provider_read(): array
+    {
+        return [
+            [
+                'dataTest' => self::retrieveGenericDataTest()
+            ]
+        ];
+    }
+
+    public static function provider_readToShare(): array
+    {
+        $dataExpected = self::retrieveGenericDataTest();
+        $dataExpected['state_literal'] = '<html lang="es"></html>';
+
+        return [
+            [
+                'dataExpected' => $dataExpected
+            ]
+        ];
+    }
+
+    private static function retrieveGenericDataTest(): array
+    {
+        return [
+            'id' => 1,
+            'institutionId' => 1,
+            'name' => 'name',
+            'address' => 'address',
+            'phone' => 'phone',
+            'email' => 'test@test.com',
+            'observations' => 'observations',
+            'search' => 'search',
+            'state' => 1,
+            'createdAt' => '2024-06-24 08:23:00',
+            'updatedAt' => '2024-06-24 08:23:00',
+        ];
+    }
+}
